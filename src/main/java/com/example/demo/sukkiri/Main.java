aa4fa5b6ef24b1534e65afb8b0503ec92e0d246a
Yuzunoha <studysteadystandby@gmail.com>
2023-02-11 11:57:21 +0900

    スッキリ

---
+++ src/main/java/com/example/demo/sukkiri/Main.java
@@ -0,0 +1,18 @@
+package com.example.demo.sukkiri;
+
+import com.example.demo.sukkiri.comment.Zenhan;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(calcTriangleArea(10, 5));
+        System.out.println(calcCircleArea(5.0));
+    }
+
+    public static double calcTriangleArea(double bottom, double height) {
+        return bottom * height / 2;
+    }
+
+    public static double calcCircleArea(double radius) {
+        return radius * radius * 3.14;
+    }
+}
---
+++ src/main/java/com/example/demo/sukkiri/comment/Zenhan.java
@@ -0,0 +1,11 @@
+package com.example.demo.sukkiri.comment;
+
+public class Zenhan {
+    public static void doWarusa() {
+        System.out.println("きなこ。金です。");
+    }
+
+    public static void doTogame() {
+        System.out.println("だめです。");
+    }
+}
