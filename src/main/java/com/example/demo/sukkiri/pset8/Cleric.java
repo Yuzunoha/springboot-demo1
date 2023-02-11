787081d39bd7d5ae13b1e64d4f12b09213fac8bc
Yuzunoha <studysteadystandby@gmail.com>
2023-02-11 14:15:24 +0900

    くれりっくです

---
+++ src/main/java/com/example/demo/sukkiri/pset8/Cleric.java
@@ -0,0 +1,5 @@
+package com.example.demo.sukkiri.pset8;
+
+public class Cleric {
+    public static String x = "クレリックです";
+}
---
+++ src/main/java/com/example/demo/sukkiri/pset8/Main.java
@@ -0,0 +1,7 @@
+package com.example.demo.sukkiri.pset8;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Cleric.x);
+    }
+}
