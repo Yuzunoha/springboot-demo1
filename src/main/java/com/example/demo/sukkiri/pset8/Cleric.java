2f7a75667dee222c122bf0a45d4dc8be2dedb18d
Yuzunoha <studysteadystandby@gmail.com>
2023-02-11 15:34:59 +0900

    pray

--- src/main/java/com/example/demo/sukkiri/pset8/Cleric.java
+++ src/main/java/com/example/demo/sukkiri/pset8/Cleric.java
@@ -1,5 +1,30 @@
 package com.example.demo.sukkiri.pset8;
 
+import java.util.Random;
+
 public class Cleric {
-    public static String x = "クレリックです";
+    private String name;
+    private int hp = 10;
+    private int mp = 10;
+    private final int MAX_HP = 50;
+    private final int MAX_MP = 50;
+
+    private final Random random = new Random();
+
+    public void selfAid() {
+        this.mp -= 5;
+        this.hp = this.MAX_HP;
+    }
+
+    public int pray(int sec) {
+        // 実際に回復したmpの量: sec + (0~2)
+        // ただしmpはMAX_MPを超えない
+        int newMp = this.mp + sec + this.random.nextInt(0, 3);
+        if (this.MAX_MP < newMp) {
+            newMp = this.MAX_MP;
+        }
+        int diff = newMp - this.mp;
+        this.mp = newMp;
+        return diff;
+    }
 }
--- src/main/java/com/example/demo/sukkiri/pset8/Main.java
+++ src/main/java/com/example/demo/sukkiri/pset8/Main.java
@@ -1,7 +1,21 @@
 package com.example.demo.sukkiri.pset8;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Cleric.x);
+        Cleric c = new Cleric();
+        int min, max;
+        min = max = 100;
+        for (int i = 0; i < 10000; i++) {
+            int x = c.pray(99);
+            if (x < min) {
+                min = x;
+            }
+            if (max < x) {
+                max = x;
+            }
+        }
+        System.out.println(min + ":" + max);
     }
 }
