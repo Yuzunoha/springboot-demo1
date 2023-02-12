81874aedc736da2241bf855ae93024b7ca8f7751
Yuzunoha <studysteadystandby@gmail.com>
2023-02-12 14:51:23 +0900

    SSLエラーでlocalstackに書き込めない

--- src/main/java/com/example/demo/controller/KinesisController.java
+++ src/main/java/com/example/demo/controller/KinesisController.java
@@ -1,15 +1,31 @@
 package com.example.demo.controller;
 
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.kinesis.KinesisSender;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/kinesis")
 public class KinesisController {
 
+    private final KinesisSender kinesisSender;
+
     @GetMapping("/send")
     public String send() {
-        return "sendです。";
+        try {
+            kinesisSender.run();
+        } catch (ExecutionException e) {
+            return "ExecutionExceptionが発生しました。";
+        } catch (InterruptedException e) {
+            return "InterruptedExceptionが発生しました。";
+        }
+        return "sendで例外は発生しませんでした。";
     }
 }
