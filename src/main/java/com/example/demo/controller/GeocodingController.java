8cb06e357d3e11e16a7f873fe212e9c0049c0e12
Yuzunoha <studysteadystandby@gmail.com>
2023-07-26 22:09:59 +0900

    つながる

--- src/main/java/com/example/demo/controller/GeocodingController.java
+++ src/main/java/com/example/demo/controller/GeocodingController.java
@@ -1,5 +1,12 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/geo")
 public class GeocodingController {
 
-    // @Value("${org.yyama.hoge}")
+    @Value("${apikey}")
+    private String apikey;
 
-    @GetMapping("/")
+    @GetMapping("")
     public String index() {
+        var client = HttpClient.newHttpClient();
+        var url = "https://dummyapi.com";
+        var request = HttpRequest.newBuilder(
+                URI.create(url))
+                .header("X-API-Key", apikey)
+                .header("accept", "application/json")
+                .build();
+
+        try {
+            var response = client.send(request, BodyHandlers.ofString());
+            System.out.println(response);
+        } catch (IOException | InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         return "郵便番号";
     }
 }
